3fd1dee00d84191291754c6f76e0d6d225c6bcee
David Schneider <mail@schnav.id>
2023-11-20 20:08:00 +0100

    simple shooting

--- working-title-player/src/lib.rs
+++ working-title-player/src/lib.rs
@@ -1,6 +1,7 @@
 use bevy::input::mouse::MouseMotion;
 use bevy::prelude::*;
 use bevy::window::{CursorGrabMode, PrimaryWindow};
+use bevy_obj::ObjPlugin;
 use bevy_rapier3d::prelude::*;
 use working_title_core::GameState;
 
@@ -8,12 +9,19 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_player)
+        app.add_plugins(ObjPlugin)
+            .add_systems(Startup, setup_player)
             .add_systems(
                 Update,
-                (handle_movement_input, handle_mouse, handle_other_input)
+                (
+                    handle_movement_input,
+                    handle_mouse,
+                    handle_other_input,
+                    handle_shooting,
+                )
                     .run_if(in_state(GameState::FirstPerson)),
             )
+            .add_systems(Update, bullet_collision)
             .add_systems(PreUpdate, reset_movement_input)
             .add_systems(PostUpdate, handle_movement_physics)
             .add_systems(OnEnter(GameState::FirstPerson), grab_cursor)
@@ -35,7 +43,11 @@ pub fn release_cursor(mut query: Query<&mut Window, With<PrimaryWindow>>) {
     }
 }
 
-pub fn setup_player(mut commands: Commands) {
+pub fn setup_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     commands
         .spawn((
             TransformBundle::from_transform(Transform::from_xyz(0.0, 1.0, 0.0)),
@@ -62,19 +74,34 @@ pub fn setup_player(mut commands: Commands) {
             // },
             ReadMassProperties::default(),
             PlayerController::default(),
+            InheritedVisibility::VISIBLE,
         ))
         .with_children(|builder| {
-            builder.spawn((
-                Camera3dBundle {
-                    transform: Transform::from_xyz(0.0, 0.25, 0.0),
-                    camera: Camera {
-                        // is_active: false,
+            builder
+                .spawn((
+                    Camera3dBundle {
+                        transform: Transform::from_xyz(0.0, 0.25, 0.0),
+                        camera: Camera {
+                            // is_active: false,
+                            ..Default::default()
+                        },
                         ..Default::default()
                     },
-                    ..Default::default()
-                },
-                PlayerCam,
-            ));
+                    PlayerCam,
+                    InheritedVisibility::VISIBLE,
+                ))
+                .with_children(|builder| {
+                    builder.spawn(PbrBundle {
+                        transform: Transform::from_xyz(0.25, -0.15, -0.4)
+                            .with_rotation(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2)),
+                        mesh: asset_server.load("models/mosin.gltf#Mesh0/Primitive0"),
+                        material: materials.add(StandardMaterial {
+                            base_color_texture: Some(asset_server.load("models/2_1.BMP")),
+                            ..Default::default()
+                        }),
+                        ..Default::default()
+                    });
+                });
         });
 }
 
@@ -199,6 +226,59 @@ fn handle_movement_physics(
         }
 
         controller.last_target_velocity = target_velocity;
+    });
+}
+
+fn handle_shooting(
+    mouse: Res<Input<MouseButton>>,
+    camera_query: Query<&GlobalTransform, With<PlayerCam>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let camera_transform = camera_query.get_single().unwrap();
+
+    let mut transform = camera_transform.compute_transform();
+
+    let forward = transform.forward();
+
+    transform.translation += forward;
+
+    if mouse.just_pressed(MouseButton::Left) {
+        commands.spawn((
+            Bullet,
+            RigidBody::Dynamic,
+            Collider::ball(0.025),
+            Velocity::linear(forward * 200.0),
+            Ccd::enabled(),
+            PbrBundle {
+                transform,
+                mesh: meshes.add(
+                    shape::UVSphere {
+                        radius: 0.025,
+                        ..Default::default()
+                    }
+                    .into(),
+                ),
+                material: materials.add(Color::RED.into()),
+                ..Default::default()
+            },
+        ));
+    }
+}
+
+#[derive(Component)]
+struct Bullet;
+
+fn bullet_collision(
+    query: Query<Entity, With<Bullet>>,
+    ctx: Res<RapierContext>,
+    mut commands: Commands,
+) {
+    query.for_each(|entity| {
+        if ctx.contacts_with(entity).next().is_some() {
+            commands.entity(entity).despawn();
+        }
     });
 }
 
